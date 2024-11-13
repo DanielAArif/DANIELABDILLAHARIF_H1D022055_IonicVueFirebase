567200d0dbaca92232d30f2e924676c34dbf6932
DanielAArif <daniel.arif@mhs.unsoed.ac.id>
2024-11-13 20:12:33 +0700

    Initial commit

---
+++ src/utils/firebase.ts
@@ -0,0 +1,18 @@
+// src/utils/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCQ15CTIyh7UbfUDb5GBeYVbhTJux69Fv4",
+    authDomain: "vue-firebase-d7a12.firebaseapp.com",
+    projectId: "vue-firebase-d7a12",
+    storageBucket: "vue-firebase-d7a12.firebasestorage.app",
+    messagingSenderId: "985093640397",
+    appId: "1:985093640397:web:249da801ddd9b7be7ed984",
+};
+
+const firebase = initializeApp(firebaseConfig);
+const auth = getAuth(firebase);
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, googleProvider };
